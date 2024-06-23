3ef3b5e71264f867fc03d556ca5d2a36be0182d5
yoons <yoonseok.bae98@dotincorp.com>
2024-06-23 23:10:37 +0900

    feat: create plugins registration script
    - Implement register function to initialize plugins with the Vue app instance
    - Ensure all plugins are automatically included and used in the app

--- src/app/plugins/index.js
+++ src/app/plugins/index.js
@@ -1,15 +1,17 @@
-import { App } from "vue";
-
+import router from "@/app/plugins/router";
 import CKEditor from "@ckeditor/ckeditor5-vue";
+import vuetify from "@/app/plugins/vuetify";
+import meta from "meta";
 
 /**
  * Register package. Automatically included in
- * @param {App} app - The Vue app instance
+ * @param app - The Vue app instance
  */
 function register(app) {
+	app.use(router);
 	app.use(CKEditor);
+	app.use(vuetify);
+	app.use(meta);
 }
 
-export default {
-	register,
-}
+export default { register }
