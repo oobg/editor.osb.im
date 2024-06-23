d43a81616aef0244aa880641423b96c1dae49049
yoons <yoonseok.bae98@dotincorp.com>
2024-06-24 00:06:04 +0900

    feat: implement CKEditor integration in ckeditor.vue
    - Import and set up ClassicEditor from '@ckeditor/ckeditor5-build-classic'
    - Create a reactive editorData to hold the editor content
    - Configure CKEditor toolbar with heading, bold, and italic options
    - Implement onReady function to customize editor's UI when ready
    - Add a template to render CKEditor within a Vuetify container

--- src/pages/ckeditor.vue
+++ src/pages/ckeditor.vue
@@ -1,6 +1,26 @@
 <script setup>
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { ref } from 'vue';
+
+const editor = ClassicEditor;
+const editorData = ref('<p>Type here...</p>');
+
+const config = {
+  toolbar: ["heading", "|", "bold", "italic"],
+};
+
+function onReady(editor) {
+  const editorElement = editor.ui.view.editable.element;
+  editorElement.style.height = '500px';
+}
 </script>
 
 <template>
-  <div>에디터다능</div>
+  <v-container>
+    <h1>CKEditor</h1>
+    <ckeditor :editor="editor" v-model="editorData" :config="config" @ready="onReady" />
+  </v-container>
 </template>
+
+<style>
+</style>
