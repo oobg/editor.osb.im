0a56bced028462ac5f05916675e942cdf130f00f
yoons <yoonseok.bae98@dotincorp.com>
2024-06-25 00:53:23 +0900

    feat: update ckeditor.vue with CKEditor integration and styles
    - Import and set up ClassicEditor from '@ckeditor/ckeditor5-build-classic'
    - Create a reactive editorData to hold the editor content
    - Import CKEditor configuration from '@/features/ckeditor/config.js'
    - Implement onReady function to log when the editor is ready
    - Add a template to render CKEditor within a Vuetify container
    - Add SCSS styles for v-container and ck-container for layout and styling

---
+++ src/features/ckeditor/config.js
@@ -0,0 +1,22 @@
+const config = {
+	toolbar: {
+		items: [
+			'heading', '|',
+			'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', '|',
+			'undo', 'redo'
+		]
+	},
+	language: 'en',
+	image: {
+		toolbar: [
+			'imageTextAlternative', 'imageStyle:full', 'imageStyle:side'
+		]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn', 'tableRow', 'mergeTableCells'
+		]
+	},
+}
+
+export default config;
--- src/pages/ckeditor.vue
+++ src/pages/ckeditor.vue
@@ -1,26 +1,62 @@
 <script setup>
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { ref } from 'vue';
+import config from "@/features/ckeditor/config.js";
 
 const editor = ClassicEditor;
-const editorData = ref('<p>Type here...</p>');
-
-const config = {
-  toolbar: ["heading", "|", "bold", "italic"],
-};
+const editorData = ref("");
 
 function onReady(editor) {
-  const editorElement = editor.ui.view.editable.element;
-  editorElement.style.height = '500px';
+  console.log("Editor is ready to use!", editor);
 }
 </script>
 
 <template>
   <v-container>
     <h1>CKEditor</h1>
-    <ckeditor :editor="editor" v-model="editorData" :config="config" @ready="onReady" />
+    <div class="ck-container">
+      <ckeditor :editor="editor" v-model="editorData" :config="config" @ready="onReady" />
+    </div>
   </v-container>
 </template>
 
-<style>
+<style lang="scss">
+.v-container {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  gap: 10px;
+}
+
+.ck-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  padding: 10px;
+  background-color: whitesmoke;
+}
+
+.ck-editor {
+  flex: 1 1 auto;
+  display: flex;
+  flex-direction: column;
+  &__main {
+    flex: 1 1 auto;
+    .ck-content,
+    .ck-source-editing-area {
+      height: 100%;
+      min-height: calc(100vh - 375px);
+      max-height: calc(100vh - 375px);
+
+      textarea {
+        overflow: auto !important;
+      }
+    }
+  }
+}
 </style>
