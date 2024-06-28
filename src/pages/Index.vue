642252e55e301ed444ca09b6c1bc635351d5c815
yoons <yoonseok.bae98@dotincorp.com>
2024-06-28 23:17:29 +0900

    fix: update navigation path in main page button
    - Change the navigation path in the button to "/ckeditor/virtual-scroll"

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,7 +10,7 @@ useMeta({
 <template>
   <v-container>
     <router-link to="/ckeditor">
-      <v-btn class="mx-4 white--text" elevation="2" x-large rounded color="deep-purple darken-1" to="/ckeditor">
+      <v-btn class="mx-4 white--text" elevation="2" x-large rounded color="deep-purple darken-1" to="/ckeditor/virtual-scroll">
         ckeditor
       </v-btn>
     </router-link>
