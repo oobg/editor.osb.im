ac53f0f39f816413febaf883d2f104122c94d899
yoons <yoonseok.bae98@dotincorp.com>
2024-07-09 23:29:29 +0900

    refactor: enhance and refactor $observer logic
    - Update IntersectionObserver options with dynamic rootMargin
    - Add rmo (rootMutationObserver) to observe CKEditor root element changes
    - Implement change method to reinitialize IntersectionObserver with new rootMargin
    - Ensure observers are properly managed and cleaned up on route changes
    - Rename autoDisconnect to autoDisconnectOnRouteChange for clarity
    - Improve overall observer logic and DOM change handling

--- src/features/ckeditor/api/virtual-scroll/private/observer.js
+++ src/features/ckeditor/api/virtual-scroll/private/observer.js
@@ -5,19 +5,40 @@ import router from "@/app/plugins/router.js"
 const $observer = {
 	options: {
 		root: null,
-		rootMargin: "1000% 0px",
+		rootMargin: "0px",
 		threshold: 0,
 	},
 	io: null,
 	mo: null,
+	rmo: null,
 
 	async init() {
+		// Intersection Observer 초기화
 		this.root = await $editor.getEditorDom();
 		this.options.root = this.root;
+		this.options.rootMargin = "1000% 0px";
 		this.io = new IntersectionObserver(this.intersectionHandler.bind(this), this.options);
+
+		// CKEditor 자식 노드 감시
 		this.mo = new MutationObserver(this.mutationHandler.bind(this));
 		this.mo.observe(this.root, { childList: true });
-		this.autoDisconnect();
+
+		// CKEditor root element 감시
+		this.rmo = new MutationObserver(this.rootMutationObserver.bind(this));
+		this.rmo.observe(this.root.parentNode, { childList: true });
+
+		// 라우터 변경 시 옵저버 해제
+		this.autoDisconnectOnRouteChange();
+	},
+
+	change(isAccessibility) {
+		this.io.takeRecords();
+		this.io.disconnect();
+		this.options.rootMargin = isAccessibility ? "0px" : "1000% 0px";
+
+		this.io = new IntersectionObserver(this.intersectionHandler.bind(this), this.options);
+		const elements = Array.from(this.root.children);
+		elements.forEach(element => this.observe(element));
 	},
 
 	/**
@@ -45,6 +66,14 @@ const $observer = {
 		})
 	},
 
+	rootMutationObserver(mutations) {
+		mutations.removedNodes.forEach(node => {
+			if (node === this.root) {
+				this.disconnect();
+			}
+		});
+	},
+
 	/**
 	 * 노드 목록을 순회하며 콜백 함수 실행
 	 * @param {NodeList} nodes
@@ -63,13 +92,15 @@ const $observer = {
 	},
 
 	disconnect() {
-		this.io?.disconnect();
-		this.mo?.disconnect();
-		this.io = null;
-		this.mo = null;
+		const observerList = ["io", "mo", "rmo"];
+		observerList.forEach(observer => {
+			this[observer]?.takeRecords();
+			this[observer]?.disconnect();
+			this[observer] = null;
+		});
 	},
 
-	autoDisconnect() {
+	autoDisconnectOnRouteChange() {
 		router.beforeEach((to, from, next) => {
 			this.disconnect();
 			next();
