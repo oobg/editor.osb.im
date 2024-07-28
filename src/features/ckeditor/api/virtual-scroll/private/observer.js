a826da668fc339a2e050b31967e21a36176d2312
baewoong <yoonseok.bae98@gmail.com>
2024-07-28 15:31:47 +0900

    refactor: Improve code readability and add comments for observer functions
    - Added detailed comments for better understanding of each function.
    - Renamed and documented the `init`, `connect`, and `disconnect` functions.
    - Documented the `processNodes`, `observe`, `unobserve`, and `validate` functions.
    - Improved code structure and readability by adding meaningful comments.

--- src/features/ckeditor/api/virtual-scroll/private/observer.js
+++ src/features/ckeditor/api/virtual-scroll/private/observer.js
@@ -13,14 +13,23 @@ const options = {
 	delay: 100,
 };
 
+/**
+ * IntersectionObserver config root 설정
+ */
 const init = () => options.root = $editor.document.getRoot();
 
+/**
+ * IntersectionObserver와 MutationObserver 연결
+ */
 const connect = () => {
 	observers.io = new IntersectionObserver(intersectionHandler, options);
 	observers.mo = new MutationObserver(mutationHandler);
 	observers.mo.observe(options.root, { childList: true });
 }
 
+/**
+ * IntersectionObserver와 MutationObserver 해제
+ */
 const disconnect = () => {
 	for (const name in observers) {
 		if (!observers[name]) continue;
@@ -53,9 +62,33 @@ const mutationHandler = (mutations) => {
 	});
 }
 
+/**
+ * NodeList 순회하며 콜백 함수 실행
+ * @param {NodeList} nodes
+ * @param {Function} action
+ * @returns {*}
+ */
 const processNodes = (nodes, action) => nodes.forEach(node => node.nodeType === 1 && action(node));
+
+/**
+ * target element를 IntersectionObserver에 등록
+ * @param {Element} node
+ * @returns {void|*}
+ */
 const observe = (node) => validate(node) && observers.io.observe(node);
+
+/**
+ * target element를 IntersectionObserver에서 해제
+ * @param {Element} node
+ * @returns {void|*}
+ */
 const unobserve = (node) => observers.io.unobserve(node);
+
+/**
+ * target element 유효성 검사
+ * @param {Element} node
+ * @returns {boolean}
+ */
 const validate = (node) => !node.querySelector("img") && !node.classList.contains("ck-fake-selection-container");
 
 export default {
